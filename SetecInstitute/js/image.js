4b6fb2456e15f618f27427d8f4743745e5cbb783
seyhavorn <vornseyha4758@gmail.com>
2022-10-19 18:31:18 +0700

    update change setect modules

--- SetecInstitute/js/image.js
+++ SetecInstitute/js/image.js
@@ -1,35 +1,37 @@
 const image = [
   {
     id:1,
-    imageUrl: '../AsiaEuroUniversity/images/img.jpg'
+    imageUrl: './image/img.jpg'
   },
   {
     id:2,
-    imageUrl: '../AsiaEuroUniversity/images/img1.jpg'
+    imageUrl: './image/img1.jpg',
   },
   {
     id:3,
-    imageUrl: '../AsiaEuroUniversity/images/img2.jpg'
+    imageUrl: './image/img2.jpg'
   },
   {
     id:4,
-    imageUrl: '../AsiaEuroUniversity/images/img3.jpg'
+    imageUrl: './image/img3.jpg'
   },
   {
     id:5,
-    imageUrl: '../AsiaEuroUniversity/images/img4.jpg'
+    imageUrl: './image/img4.jpg'
   },
   {
     id:6,
-    imageUrl: '../AsiaEuroUniversity/images/img5.jpg'
+    imageUrl: './image/img5.jpg'
   },
 ];
 
 const imageScript = document.getElementById('image-script');
-var txt = [];
-for (i = 0; i < image.length; i++) {
-    txt += `<div class="box ${image[i].id}">
-              <img src="${image[i].imageUrl}" class="image-detail" alt="">
-            </div>`;
-}
-imageScript.innerHTML = txt;
+imageScript.innerHTML= `
+                          ${image?.map((image) => {
+                            return `<div class="box ${image?.id}">
+                                      <img src="${image?.imageUrl}" class="image-detail" alt="">
+                                    </div>`;
+                          }).join(" ")};
+                        `;
+
+                        
