886f3e3878dbe2b2dc0d34892a622ccab907a04e
dara61 <huydara7756161@gmail.com>
2022-11-07 23:01:21 +0700

    image done - dara

--- pages/rule/image.js
+++ pages/rule/image.js
@@ -1,33 +1,35 @@
 const image = [
     {
         id: 1,
-        imageUrl: './pciture/b12.jpg'
+        imageUrl: './picture/b12.jpg'
     },
     {
         id: 2,
-        imageUrl: './pciture/b22.jpg'
+        imageUrl: './picture/b22.jpg',
     },
     {
         id: 3,
-        imageUrl: './pciture/b32.jpg'
+        imageUrl: './picture/b32.jpg'
     },
     {
         id: 4,
-        imageUrl: './pciture/b4_new_building_in_20042.jpg'
+        imageUrl: './picture/b4_new_building_in_20042.jpg'
     },
     {
         id: 5,
-        imageUrl: './pciture/b52.jpg'
+        imageUrl: './picture/b52.jpg'
     },
     {
         id: 6,
-        imageUrl: './pciture/map.jpg'
-    }
+        imageUrl: './picture/map.jpg'
+    },
 ];
 
 const imageScript = document.getElementById('image-script');
-imageScript.innerHTML = `${image?.map((image) => {
+imageScript.innerHTML = `
+                        ${image?.map((image) => {
     return `<div class="box ${image?.id}">
-                <img src="${image?.imageUrl}" class="image-detail" alt="">
-            </div>`
+                                    <img src="${image?.imageUrl}" class="image-detail" alt="">
+                                  </div>`
 }).join(" ")}`;
+
